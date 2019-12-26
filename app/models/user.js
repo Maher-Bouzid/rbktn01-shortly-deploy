749c63ce0a0f989451c8366248f187a7dbed1cf1
abdelsalamshahlol <abdulsalamshahlol@gmail.com>
2019-12-26 11:14:59 +0100

    user model complete

--- app/models/user.js
+++ app/models/user.js
@@ -3,27 +3,6 @@ var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 const mongoose = require('mongoose');
 
-// var User = db.Model.extend({
-//   tableName: 'users',
-//   hasTimestamps: true,
-//   initialize: function() {
-//     this.on('creating', this.hashPassword);
-//   },
-//   comparePassword: function(attemptedPassword, callback) {
-//     bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-//       callback(isMatch);
-//     });
-//   },
-//   hashPassword: function() {
-//     var cipher = Promise.promisify(bcrypt.hash);
-//     return cipher(this.get('password'), null, null).bind(this)
-//       .then(function(hash) {
-//         this.set('password', hash);
-//       });
-//   }
-// });
-
-// module.exports = User;
 class User {
   constructor(data) {
     this.model = mongoose.model('User', db.user);
@@ -36,7 +15,7 @@ class User {
   }
 
   find() {
-    return this.model.find({ username: this._data.username });
+    return this.model.findOne({ username: this._data.username });
   }
 
   comparePassword(attemptedPassword, actualPassword, callback) {
